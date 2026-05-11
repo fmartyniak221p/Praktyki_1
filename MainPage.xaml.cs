cc6089a0a0c44d9843f80769147739fd9940e37e
fmartyniak221p <fmartyniak_221p@e.zseeim.edu.pl>
2026-05-11 11:29:14 +0200

    dodanie plików następnego zadania

---
+++ MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiApp12
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> ListaNotatek { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ListaNotatek = new ObservableCollection<string>
+        {
+            "Zakupy: chleb, masło, ser",
+            "Do zrobienia: obiad, umyć podłogi",
+            "weekend: kino, spacer z psem"
+        };
+
+            WidokListyNotatek.ItemsSource = ListaNotatek;
+        }
+
+        private void KliknieciePrzyciskuDodaj(object sender, EventArgs e)
+        {
+            string trescNotatki = PoleTekstowe.Text;
+
+            if (!string.IsNullOrWhiteSpace(trescNotatki))
+            {
+                ListaNotatek.Add(trescNotatki);
+
+                PoleTekstowe.Text = string.Empty;
+            }
+        }
+    }
+}
