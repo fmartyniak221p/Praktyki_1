9842e6af6c38d7781144123989048ce2a85e9985
fmartyniak221p <fmartyniak_221p@e.zseeim.edu.pl>
2026-05-12 11:28:30 +0200

    dodanie

---
+++ Zadanie5/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace MauiApp13
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            LblKomunikat.Text = "Autor: 00000000000";
+        }
+
+        private void OnZatwierdzClicked(object sender, EventArgs e)
+        {
+            string email = EntEmail.Text ?? "";
+            string haslo = EntHaslo.Text ?? "";
+            string powtorzHaslo = EntPowtorzHaslo.Text ?? "";
+
+
+            if (!email.Contains("@"))
+            {
+                LblKomunikat.Text = "Nieprawidłowy adres e-mail";
+                return;
+            }
+
+
+            if (haslo != powtorzHaslo)
+            {
+                LblKomunikat.Text = "Hasła się różnią";
+                return;
+            }
+
+
+            LblKomunikat.Text = $"Witaj {email}";
+        }
+    }
+}
