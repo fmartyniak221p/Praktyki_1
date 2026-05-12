c0b959b44a830bd007c2e294a7cb675f3de5c5d4
fmartyniak221p <fmartyniak_221p@e.zseeim.edu.pl>
2026-05-12 11:04:25 +0200

    Dodanie plików zadania

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace MauiApp13
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ZmienKolor(object sender, ValueChangedEventArgs e)
+        {
+            int r = (int)SliderR.Value;
+            int g = (int)SliderG.Value;
+            int b = (int)SliderB.Value;
+
+
+            LabelR.Text = r.ToString();
+            LabelG.Text = g.ToString();
+            LabelB.Text = b.ToString();
+
+
+            DuzyProstokat.Color = Color.FromRgb(r, g, b);
+        }
+
+        private void PobierzKolor(object sender, EventArgs e)
+        {
+            int r = (int)SliderR.Value;
+            int g = (int)SliderG.Value;
+            int b = (int)SliderB.Value;
+
+
+            EtykietaWynik.Text = $"{r}, {g}, {b}";
+            EtykietaWynik.BackgroundColor = Color.FromRgb(r, g, b);
+        }
+    }
+}
